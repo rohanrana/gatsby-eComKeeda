0fe8af3eae34601afa53f2ab4973da2b9fba2d10
dvshah26 <46349256+dvshah26@users.noreply.github.com>
2020-05-25 15:24:21 +0530

    test

--- src/components/image.js
+++ src/components/image.js
@@ -8,8 +8,7 @@ import errorImage from "../images/placeholder.png"
 const OptimizedImage = ({ src, style }) => (
   <Img
     placeholder={loadingImage}
-    // src={src}
-    src={`http://34.67.49.241/wp-content/uploads/2020/05/a613a441-ebb9-316d-9580-74e06cfdb297.jpg`}
+    src={src}
     style={style}
     cache
     error={errorImage}
